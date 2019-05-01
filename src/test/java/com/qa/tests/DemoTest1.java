3c0732a02619cb35ae7c4d891c741be7f244b956
xMNS2025 <you@example.com>
2019-05-01 17:03:30 -0500

    first git commit for my selenium code

---
+++ src/test/java/com/qa/tests/DemoTest1.java
@@ -0,0 +1,39 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	@Test
+	public void sum(){
+		System.out.println("sum");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub(){
+		System.out.println("sub");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10,b-a);
+	}
+	
+	@Test
+	public void multiplication(){
+		System.out.println("mul");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	@Test
+	public void div(){
+		System.out.println("div");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+}
